320489546c7c438aff6776172640592b3d74344a
elmian007 <h4bzbcbgfg@privaterelay.appleid.com>
2026-07-12 01:08:27 +0200

    Create database.js

---
+++ database.js
@@ -0,0 +1,92 @@
+let db;
+
+
+function openDatabase(){
+
+return new Promise((resolve,reject)=>{
+
+let request=indexedDB.open(
+"WettenNLDatabase",
+1
+);
+
+
+request.onupgradeneeded=function(event){
+
+db=event.target.result;
+
+
+if(!db.objectStoreNames.contains("artikelen")){
+
+db.createObjectStore(
+"artikelen",
+{
+keyPath:"id"
+}
+);
+
+}
+
+};
+
+
+request.onsuccess=function(event){
+
+db=event.target.result;
+resolve(db);
+
+};
+
+
+request.onerror=function(){
+
+reject("Database fout");
+
+};
+
+
+});
+
+}
+
+
+
+function artikelOpslaan(artikel){
+
+let transactie=db.transaction(
+["artikelen"],
+"readwrite"
+);
+
+let store=transactie.objectStore("artikelen");
+
+store.put(artikel);
+
+}
+
+
+
+function alleArtikelen(){
+
+return new Promise((resolve)=>{
+
+let transactie=db.transaction(
+["artikelen"],
+"readonly"
+);
+
+let store=transactie.objectStore("artikelen");
+
+let request=store.getAll();
+
+
+request.onsuccess=function(){
+
+resolve(request.result);
+
+};
+
+
+});
+
+}
