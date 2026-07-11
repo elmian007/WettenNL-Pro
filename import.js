d5a7f0a1a243ec5fb648d794a9d94f1bfa99eb4e
elmian007 <h4bzbcbgfg@privaterelay.appleid.com>
2026-07-12 01:28:21 +0200

    Update import.js

--- import.js
+++ import.js
@@ -1,26 +1,59 @@
 async function laadWetData(){
 
-    const antwoord = await fetch("data/wetten.json");
+const antwoord = await fetch(
+"data/wetten.json"
+);
 
-    const gegevens = await antwoord.json();
+const gegevens = await antwoord.json();
 
-    for(const wet of gegevens.wetten){
 
-        for(const artikel of wet.artikelen){
+for(const wet of gegevens.wetten){
 
-            const record = {
-                id: wet.code + "-" + artikel.nummer,
-                wet: wet.naam,
-                code: wet.code,
-                nummer: artikel.nummer,
-                titel: artikel.titel,
-                tekst: artikel.tekst
-            };
 
-            artikelOpslaan(record);
-        }
-    }
+for(const onderdeel of wet.onderdelen){
 
-    console.log("Wetgegevens geladen");
+
+for(const artikel of onderdeel.artikelen){
+
+
+const record={
+
+id:
+wet.code+"-"+artikel.nummer,
+
+wet:
+wet.naam,
+
+code:
+wet.code,
+
+hoofdstuk:
+onderdeel.naam,
+
+nummer:
+artikel.nummer,
+
+titel:
+artikel.titel,
+
+tekst:
+artikel.tekst
+
+};
+
+
+artikelOpslaan(record);
+
+
+}
+
+}
+
+}
+
+
+console.log(
+"Wetgeving geïmporteerd"
+);
 
 }
