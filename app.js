6f98f3e32d3e233e68d92e76383de45d978789f1
elmian007 <h4bzbcbgfg@privaterelay.appleid.com>
2026-07-12 00:53:47 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const wetten = {
+
+Sr:[
+{
+artikel:"300",
+titel:"Mishandeling",
+tekst:"Hij die opzettelijk iemand mishandelt..."
+},
+{
+artikel:"310",
+titel:"Diefstal",
+tekst:"Hij die enig goed dat geheel of ten dele aan een ander toebehoort..."
+}
+],
+
+Sv:[],
+WVW:[],
+RVV:[],
+WWM:[],
+Opium:[],
+Awb:[]
+
+};
+
+
+function openWet(wet){
+
+const resultaat=document.getElementById("resultaat");
+
+const artikelen=wetten[wet];
+
+if(artikelen.length===0){
+
+resultaat.innerHTML="<p>Nog geen artikelen geladen.</p>";
+
+return;
+
+}
+
+let html="";
+
+artikelen.forEach(a=>{
+
+html+=`
+
+<h3>Artikel ${a.artikel}</h3>
+
+<strong>${a.titel}</strong>
+
+<p>${a.tekst}</p>
+
+<hr>
+
+`;
+
+});
+
+resultaat.innerHTML=html;
+
+}
