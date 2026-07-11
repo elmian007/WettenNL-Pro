668c805deb224ed7defd57bd4dbc9aee5d79483d
elmian007 <h4bzbcbgfg@privaterelay.appleid.com>
2026-07-12 01:32:02 +0200

    Create sync.js

---
+++ backend/sync.js
@@ -0,0 +1,31 @@
+const laatsteVersie = {
+
+    SR: "2026-01-01",
+    Sv: "2026-01-01",
+    WVW: "2026-01-01",
+    RVV: "2026-01-01",
+    WWM: "2026-01-01",
+    Opium: "2026-01-01",
+    Awb: "2026-01-01"
+
+};
+
+
+function controleerUpdates(){
+
+    console.log(
+        "Controle op wetswijzigingen gestart"
+    );
+
+
+    /*
+    
+    Hier komt later:
+
+    1. Ophalen actuele gegevens
+    2. Vergelijken versies
+    3. Nieuwe artikelen terugsturen
+
+    */
+
+}
